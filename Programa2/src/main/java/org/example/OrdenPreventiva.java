18f4f8a862217821d49f6be6ad24cfe71acd8702
Necromancer023 <giancartavia@gmail.com>
2025-12-04 20:36:02 -0600

    Update OrdenPreventiva.java

--- Programa2/src/main/java/org/example/OrdenPreventiva.java
+++ Programa2/src/main/java/org/example/OrdenPreventiva.java
@@ -4,82 +4,120 @@ import java.time.LocalDate;
 
 public class OrdenPreventiva {
 
-    private int idOrden;
-    private LocalDate fechaProgramada;
-    private LocalDate fechaEjecucion;
     public enum EstadoOrden {
         PROGRAMADA,
         EN_PROCESO,
         COMPLETADA,
         CANCELADA
     }
+
+    public enum Prioridad {
+        ALTA,
+        MEDIA,
+        BAJA
+    }
+
+    private int idOrden;
+    private LocalDate fechaProgramada;
+    private LocalDate fechaEjecucion;
+
     private EstadoOrden estado;
+    private Prioridad prioridad;             
+    private String observacionInicial;       
 
     private Equipo equipoAsociado;
     private FasePreventiva fase;
 
     private String tecnicoAsignado;
-    private String observaciones;
+    private String observacionesFinales;
 
-    // -- Constructor --
+    // --- Constructor ---
 
-    public OrdenPreventiva(int idOrden, LocalDate fechaProgramada, Equipo equipoAsociado,
-                           FasePreventiva fase, String tecnicoAsignado) {
+    public OrdenPreventiva(int idOrden, LocalDate fechaProgramada,
+                           Equipo equipoAsociado, FasePreventiva fase,
+                           String tecnicoAsignado, Prioridad prioridad,
+                           String observacionInicial) {
+
         this.idOrden = idOrden;
         this.fechaProgramada = fechaProgramada;
         this.equipoAsociado = equipoAsociado;
         this.fase = fase;
         this.tecnicoAsignado = tecnicoAsignado;
+        this.prioridad = prioridad;
+        this.observacionInicial = observacionInicial;
+
         this.estado = EstadoOrden.PROGRAMADA;
-        this.observaciones = "";
+        this.observacionesFinales = "";
     }
 
-    // -- Getters y Setters --
+    // --- Getters y Setters ---
+
     public int getIdOrden() {
         return idOrden;
     }
+
     public LocalDate getFechaProgramada() {
         return fechaProgramada;
     }
     public void setFechaProgramada(LocalDate fechaProgramada) {
         this.fechaProgramada = fechaProgramada;
     }
+
     public LocalDate getFechaEjecucion() {
         return fechaEjecucion;
     }
     public void setFechaEjecucion(LocalDate fechaEjecucion) {
         this.fechaEjecucion = fechaEjecucion;
     }
+
     public EstadoOrden getEstado() {
         return estado;
     }
     public void setEstado(EstadoOrden estado) {
         this.estado = estado;
     }
+
+    public Prioridad getPrioridad() {
+        return prioridad;
+    }
+    public void setPrioridad(Prioridad prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    public String getObservacionInicial() {
+        return observacionInicial;
+    }
+    public void setObservacionInicial(String observacionInicial) {
+        this.observacionInicial = observacionInicial;
+    }
+
     public Equipo getEquipoAsociado() {
         return equipoAsociado;
     }
+
     public FasePreventiva getFase() {
         return fase;
     }
+
     public String getTecnicoAsignado() {
         return tecnicoAsignado;
     }
     public void setTecnicoAsignado(String tecnicoAsignado) {
         this.tecnicoAsignado = tecnicoAsignado;
     }
-    public String getObservaciones() {
-        return observaciones;
+
+    public String getObservacionesFinales() {
+        return observacionesFinales;
     }
-    public void setObservaciones(String observaciones) {
-        this.observaciones = observaciones;
+    public void setObservacionesFinales(String obs) {
+        this.observacionesFinales = obs;
     }
 
-    // -- Métodos --
-
-    public void marcarComoCompletada(LocalDate fechaEjecucionReal) {
+    // --- Métodos adicionales ---
+    public void marcarComoCompletada(LocalDate fechaEjecucionReal, String observacionesFinales) {
         this.estado = EstadoOrden.COMPLETADA;
         this.fechaEjecucion = fechaEjecucionReal;
+        this.observacionesFinales = observacionesFinales;
     }
 
     @Override
@@ -89,10 +127,12 @@ public class OrdenPreventiva {
                 ", fechaProgramada=" + fechaProgramada +
                 ", fechaEjecucion=" + fechaEjecucion +
                 ", estado=" + estado +
+                ", prioridad=" + prioridad +
+                ", observacionInicial='" + observacionInicial + '\'' +
                 ", equipoAsociado=" + equipoAsociado.getDescripcion() +
                 ", fase=" + fase.getDescripcion() +
                 ", tecnicoAsignado='" + tecnicoAsignado + '\'' +
-                ", observaciones='" + observaciones + '\'' +
+                ", observacionesFinales='" + observacionesFinales + '\'' +
                 '}';
     }
 }
